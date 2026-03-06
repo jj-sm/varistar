825b369030ecbdb72d68e2dde329549dc12f139d
juanjo <gh@jjsm.science>
2026-03-06 15:00:44 -0300

    fix: workflow

--- src/varistar/viz/style.py
+++ src/varistar/viz/style.py
@@ -23,7 +23,6 @@ from __future__ import annotations
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import numpy as np
 
 
 # ---------------------------------------------------------------------------

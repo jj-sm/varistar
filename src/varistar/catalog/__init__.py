9dd5d7993bf4f07e700d564e43fc96d6ec5b0561
Juan José Sánchez Medina <gh@jjsm.science>
2026-09-03 10:54:14 -0400

    fix: init

--- src/varistar/catalog/__init__.py
+++ src/varistar/catalog/__init__.py
@@ -12,11 +12,19 @@ Quick reference
 >>> from varistar.catalog.ogle import load_dat
 >>> from varistar.catalog.generic import load_csv, from_arrays
 >>> from varistar.catalog.tess import load_fits, load_from_tic
+>>> from varistar.catalog.gaiadr3 import load_csv as gaiadr3_load_csv, GAIA_TIME_OFFSET
 """
 
 from varistar.catalog.ogle import load_dat, load_dat_directory
 from varistar.catalog.generic import load_csv, from_arrays
 from varistar.catalog.tess import load_fits, load_from_tic
+from varistar.catalog.gaia import load_gaia
+from varistar.catalog.gaiadr3 import (
+    load_csv as gaiadr3_load_csv,
+    load_csv_directory as gaiadr3_load_csv_directory,
+    parse_gaia_id,
+    GAIA_TIME_OFFSET,
+)
 
 __all__ = [
     "load_dat",
@@ -25,4 +33,8 @@ __all__ = [
     "from_arrays",
     "load_fits",
     "load_from_tic",
+    "gaiadr3_load_csv",
+    "gaiadr3_load_csv_directory",
+    "parse_gaia_id",
+    "GAIA_TIME_OFFSET",
 ]
